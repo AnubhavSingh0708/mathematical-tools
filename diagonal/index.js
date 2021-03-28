508a183db8c8b10f664031fad1a2e937caf9e934
Anubhav Singh <68738228+AnubhavSingh0708@users.noreply.github.com>
2021-03-28 21:27:08 +0530

    Update index.js

--- diagonal/index.js
+++ diagonal/index.js
@@ -1,8 +1,10 @@
 const form1 = document.querySelector('.diagonal');
-form1.addEventListener('submit', event => {
+setTimeout(function(){calculate(); }, 1000);
+function calculate() {
  var a = document.getElementById("a-value").value;
    var b = document.getElementById("b-value").value;
   var c = Math.sqrt(Math.pow(a, 2)+Math.pow(b, 2));
  document.getElementById("c-value").innerHTML = c;
  alert(c);
-}
+ setTimeout(function(){calculate(); }, 1000);
+    }
