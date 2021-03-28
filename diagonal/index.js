ab929bb6a126fae7bb149a52b81f32c5b8080ccb
Anubhav Singh <68738228+AnubhavSingh0708@users.noreply.github.com>
2021-03-28 22:02:46 +0530

    Update index.js

--- diagonal/index.js
+++ diagonal/index.js
@@ -4,6 +4,10 @@ function calculate() {
  var a = document.getElementById("a-value").value;
    var b = document.getElementById("b-value").value;
   var c = Math.sqrt(Math.pow(a, 2)+Math.pow(b, 2));
+ var d = Math.asin(a/c);
+ var e=  Math.asin(b/c);
  document.getElementById("c-value").innerHTML = c;
+  document.getElementById("d-value").innerHTML = d;
+  document.getElementById("e-value").innerHTML = e;
  setTimeout(function(){calculate(); }, 1000);
     }
