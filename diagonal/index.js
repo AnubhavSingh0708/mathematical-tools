49c5948fb619e6bc79ee9709ca1d3123772344d4
Anubhav Singh <68738228+AnubhavSingh0708@users.noreply.github.com>
2021-03-28 21:22:35 +0530

    Update index.js

--- diagonal/index.js
+++ diagonal/index.js
@@ -3,4 +3,6 @@ form1.addEventListener('submit', event => {
  var a = document.getElementById("a-value").value;
    var b = document.getElementById("b-value").value;
   var c = Math.sqrt(Math.pow(a, 2)+Math.pow(b, 2));
+ document.getElementById("c-value").innerHTML = c;
+ alert(c);
 }
