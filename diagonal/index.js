342dd635685a81b0f4a0741a77b7b04e8dd0eb05
Anubhav Singh <68738228+AnubhavSingh0708@users.noreply.github.com>
2021-03-29 10:45:36 +0530

    Update index.js

--- diagonal/index.js
+++ diagonal/index.js
@@ -7,8 +7,8 @@ function calculate() {
  var d = (180/Math.PI) * (Math.asin(a/c));
 var e=  (180/Math.PI) * (Math.asin(b/c));
 var blue;
-var green = Math.sqrt(Math.pow(a, 2)+Math.pow(b/2, 2));;
- var red =  Math.sqrt(Math.pow(a/2, 2)+Math.pow(b, 2));
+var green = Math.sqrt(Math.pow(a, 2)+Math.pow((b/2), 2));;
+ var red =  Math.sqrt(Math.pow((a/2), 2)+Math.pow(b, 2));
  document.getElementById("c-value").innerHTML = c;
   document.getElementById("d-value").innerHTML = d;
   document.getElementById("e-value").innerHTML = e;
